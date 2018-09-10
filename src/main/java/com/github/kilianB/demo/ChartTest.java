efda425e69872b7198abac413953604c54204e9a
Kilian Brachtendorf <Kilian.Brachtendorf@t-online.de>
2018-09-10 23:58:09 +0200

    Shutdown PlatformFX upon window close

--- src/main/java/com/github/kilianB/demo/ChartTest.java
+++ src/main/java/com/github/kilianB/demo/ChartTest.java
@@ -2,6 +2,7 @@ package com.github.kilianB.demo;
 
 import java.io.IOException;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -11,18 +12,21 @@ public class ChartTest extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws IOException {
-		
-	FXMLLoader loader = new FXMLLoader();
-		
-		//loader.setController(new Controller());
+
+		// exit application on window close
+		Platform.setImplicitExit(true);
+
+		FXMLLoader loader = new FXMLLoader();
+
+		// loader.setController(new Controller());
 		loader.setLocation(ChartTest.class.getResource("ChartTest.fxml"));
-		
+
 		Parent parent = loader.load();
-		
-		Scene scene = new Scene(parent,800,500);
-		 
+
+		Scene scene = new Scene(parent, 800, 500);
+
 		scene.getStylesheets().add(ChartTest.class.getResource("chart.css").toExternalForm());
-		
+
 		primaryStage.setScene(scene);
 		primaryStage.show();
 	}
